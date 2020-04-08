61883c3d82c82c05a28ca86cc63d0220522d898a
Leônidas Santos <oi@ltco.com.br>
2020-04-08 20:35:28 -0300

    Repositories CRUD and Likes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,91 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function validadeRepositoriesId(req, res, next) {
+	const { id } = req.params;
+
+	if ( !isUuid(id) )
+		return res.status(400).json({error: 'Invalid repository ID.'});
+
+	return next();
+}
+
+function isRepository(req, res, next) {
+	const { id } = req.params;
+	const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+	
+	if (repositoryIndex < 0)
+		return res.status(400).json({ error: 'Repository not found.' });
+	
+	res.locals.repositoryIndex = repositoryIndex;
+
+	return next();
+}
+
+app.use('/repositories/:id', validadeRepositoriesId, isRepository);
+
 const repositories = [];
+// const repositories = require('./database');
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => res.json(repositories));
+
+app.post("/repositories", (req, res) => {
+	const { title, url, techs } = req.body;
+
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0
+	}
+
+	repositories.push(repository);
+
+	return res.json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+app.put("/repositories/:id", (req, res) => {
+	const { id } = req.params;
+	const { repositoryIndex } = res.locals;
+	const { title, url, techs } = req.body;
+	
+	const likes = repositories[repositoryIndex].likes;
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+	const repository = {
+		id,
+		title,
+		url,
+		techs,
+		likes
+	};
+
+	repositories[repositoryIndex] = repository;
+
+	return res.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+	const { repositoryIndex } = res.locals;
+
+	repositories.splice(repositoryIndex, 1);
+
+	return res.status(204).send('');
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+	const { repositoryIndex } = res.locals;
+
+	const likes = repositories[repositoryIndex].likes += 1
+
+	return res.json({likes});
 });
 
 module.exports = app;
---
+++ src/database/index.js
@@ -0,0 +1,71 @@
+module.exports = [
+	{
+		"id": "77679136-ad4a-4348-a83e-01da2141aad0",
+		"title": "Desafio Conceitos do Node.js",
+		"url": "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+		"techs": [
+			"nodejs",
+			"desafio",
+			"bootcamp",
+			"gostack"
+		],
+		"likes": 21300
+	},
+	{
+		"id": "1f63c431-cc38-4ee3-88d3-ce5e4c312661",
+		"title": "Unform",
+		"url": "https://github.com/Rocketseat/unform",
+		"techs": [
+			"react",
+			"form",
+			"forms",
+			"react-native"
+		],
+		"likes": 456200
+	},
+	{
+		"id": "09519af9-b0af-427f-b062-14953a1285dd",
+		"title": "React Native Snippets",
+		"url": "https://github.com/Rocketseat/rocketseat-vscode-react-native-snippets",
+		"techs": [
+			"vscode",
+			"snippets",
+			"react-native"
+		],
+		"likes": 2634500
+	},
+	{
+		"id": "bad06102-dfcb-4b52-bb87-d8d96fdbb05e",
+		"title": "ReactJS Snippets",
+		"url": "https://github.com/Rocketseat/rocketseat-vscode-reactjs-snippets",
+		"techs": [
+			"vscode",
+			"snippets",
+			"reactjs"
+		],
+		"likes": 29800
+	},
+	{
+		"id": "2fdcc418-ddc3-437b-b6f2-46d216299c90",
+		"title": "Template Rocketseat Basic",
+		"url": "https://github.com/Rocketseat/react-native-template-rocketseat-basic",
+		"techs": [
+			"react-native",
+			"template",
+			"react-navigation"
+		],
+		"likes": 200
+	},
+	{
+		"id": "b4b75623-6f0f-43bd-8617-2acd067eaa01",
+		"title": "Template Rocketseat Advanced",
+		"url": "https://github.com/Rocketseat/react-native-template-rocketseat-advanced",
+		"techs": [
+			"react-native",
+			"template",
+			"redux",
+			"redux-saga"
+		],
+		"likes": 651000
+	}
+];
